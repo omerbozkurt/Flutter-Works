c86c6a6c28904a1257f54e391a33bf068c68ffd8
Ömer Bozkurt <93863644+omerbozkurt@users.noreply.github.com>
2023-03-20 11:47:49 +0300

    Row Column Stack Karışık Kullanımı

---
+++ rowcolumnstack_karisik_kullanim/android/app/src/main/kotlin/com/example/rowcolumnstack_karisik_kullanim/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rowcolumnstack_karisik_kullanim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rowcolumnstack_karisik_kullanim/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              width: 400,
+              height: 400,
+              color: Colors.red,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.white,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.black,
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
