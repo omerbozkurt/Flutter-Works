1e669cfb83b27295a4097f0f048d4b470e42493c
Ömer Bozkurt <93863644+omerbozkurt@users.noreply.github.com>
2023-03-20 14:31:02 +0300

    Hizalama İşlemleri

---
+++ hizalama_islemleri/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Hizlama İşlemleri'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(width: 80,height: 80,color: Colors.red,),
+          Container(width: 50,height: 50,color: Colors.blue,),
+          Container(width: 100,height: 100,color: Colors.green,),
+        ],
+      )
+    );
+  }
+}
